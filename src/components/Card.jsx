bc05fb78c6952e9e420c2897496aafa82d2e0369
Natalia Razmadze <natasha.razmadze@gmail.com>
2025-06-24 14:41:48 -0700

    Removed Destructuring, created local functions.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,32 @@
 import PropTypes from 'prop-types';
 import './Card.css';
 
-const Card = ({ id, message, likesCount, onLike, onDelete }) => {
-  return (
-    <div className="card">
-      <p className="card__message">{message}</p>
-      <div className="card__actions">
-        <button onClick={() => onLike(id)}>❤️ {likesCount}</button>
-        <button onClick={() => onDelete(id)}>🗑️ Delete</button>
-      </div>
-    </div>
-  );
+const Card = (props) => {
+    const handleLikeClick = () => {
+        props.onLike(props.id);
+    };
+
+    const handleDeleteClick = () => {
+        props.onDelete(props.id);
+    };
+
+    return (
+        <div className="card">
+            <p className="card__message">{props.message}</p>
+            <div className="card__actions">
+                <button onClick={handleLikeClick}>❤️ {props.likesCount}</button>
+                <button onClick={handleDeleteClick}>🗑️ Delete</button>
+            </div>
+        </div>
+    );
 };
 
 Card.propTypes = {
-  id: PropTypes.number.isRequired,
-  message: PropTypes.string.isRequired,
-  likesCount: PropTypes.number.isRequired,
-  onLike: PropTypes.func.isRequired,
-  onDelete: PropTypes.func.isRequired,
+    id: PropTypes.number.isRequired,
+    message: PropTypes.string.isRequired,
+    likesCount: PropTypes.number.isRequired,
+    onLike: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired,
 };
 
 export default Card;
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,33 +1,40 @@
+import './CardList.css';
 import PropTypes from 'prop-types';
 import Card from './Card';
 
-const CardList = ({ cards, onDeleteCard, onLikeCard }) => {
-  return (
-    <div className="card-list">
-      {cards.map((card) => (
-        <Card
-          key={card.id}
-          id={card.id}
-          message={card.message}
-          likesCount={card.likes}
-          onDelete={onDeleteCard}
-          onLike={onLikeCard}
-        />
-      ))}
-    </div>
-  );
+const CardList = (props) => {
+    const cardComponents = props.cards.map((card) => {
+        return (
+            <li key={card.id}>
+                <Card
+                    id={card.id}
+                    message={card.message}
+                    likesCount={card.likes}
+                    onDelete={props.onDeleteCard}
+                    onLike={props.onLikeCard}
+                />
+            </li>
+        );
+    });
+
+    return (
+        <section>
+            <h2 className="card-list__heading">Card List</h2>
+            <ul className="card-list">{cardComponents}</ul>
+        </section>
+    );
 };
 
 CardList.propTypes = {
-  cards: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      message: PropTypes.string.isRequired,
-      likes: PropTypes.number.isRequired,
-    })
-  ).isRequired,
-  onDeleteCard: PropTypes.func.isRequired,
-  onLikeCard: PropTypes.func.isRequired,
+    cards: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            message: PropTypes.string.isRequired,
+            likes: PropTypes.number.isRequired,
+        })
+    ).isRequired,
+    onDeleteCard: PropTypes.func.isRequired,
+    onLikeCard: PropTypes.func.isRequired,
 };
 
 export default CardList;
